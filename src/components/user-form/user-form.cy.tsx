633ffbb4e34c2dcb5a6cfed00e5aa7e88a4b4d0c
sflax <shachar.flax@gmail.com>
2023-01-10 15:32:26 +0200

    update

--- src/components/user-form/user-form.cy.tsx
+++ src/components/user-form/user-form.cy.tsx
@@ -1,28 +1,26 @@
-import UserForm from './user-form';
+import UserForm from "./user-form";
 
-describe('user-form', () => {
+describe("user-form", () => {
+  it("should display with 4 elements and button", () => {
+    cy.mount(<UserForm />);
+    cy.get("input").should("have.length", 4);
+    cy.get("button").should("have.length", 1);
+  });
 
-    it('should display with 4 elements and button', () => {
-        cy.mount(<UserForm/>);
-        cy.get('input').should('have.length', 4);
-        cy.get('button').should('have.length', 1);
+  it("should raise data", () => {
+    const submit = cy.spy().as("onUser");
+
+    cy.mount(<UserForm onUser={submit} />);
+    cy.get('button[type="submit"]').click();
+    cy.get("@onUser").should("have.been.calledWith", {
+      email: "",
+      fname: "",
+      lname: "",
+      password: "",
     });
+  });
 
-
-    it('should raise data', () => {
-        const submit = cy.spy().as('onUser');
-
-        cy.mount(<UserForm onUser={submit}/>);
-        cy.get('button[type="submit"]').click();
-        cy.get('@onUser').should('have.been.calledWith', {
-            email: '',
-            fname: '',
-            lname: '',
-            password: ''
-        });
-    });
-
-    it('should fill the text in the fields and raise the data', () => {
-        // TODO
-    });
+  it("should fill the text in the fields and raise the data", () => {
+    // TODO
+  });
 });
